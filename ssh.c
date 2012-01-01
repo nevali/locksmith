e8f579517082e5f85f7f9c0cf205843b42929043
nevali <mo@nevali.net>
2012-01-01 16:14:01 +0000

    Update copyright date

--- ssh.c
+++ ssh.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Mo McRoberts.
+ * Copyright 2011-2012 Mo McRoberts.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
